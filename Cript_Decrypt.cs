cad57bcacc3f90558b747b6db8d5f5115ff616c5
Br1 Brown <50661545+br1brown@users.noreply.github.com>
2019-12-06 15:50:47 +0100

    Algoritmo di criptazione aggiunto

---
+++ Cript_Decrypt.cs
@@ -0,0 +1,81 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Br1BrownAPI {
+	public static class ENCRYPT {
+
+		public static string PASSWORD { private get; set; }
+
+		/// <summary>
+		/// Cripta il testo passatogli
+		/// </summary>
+		/// <param name="txt">da criptare</param>
+		/// <returns>valore criptato</returns>
+		public static string Crypt_Text(string txt) {
+
+			if (string.IsNullOrEmpty(txt) || string.IsNullOrEmpty(PASSWORD))
+				return txt;
+			
+			byte[] keys;
+			using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider()) {
+				var tmp = UTF8Encoding.UTF8.GetBytes(PASSWORD);
+				keys = md5.ComputeHash(tmp);
+			}
+			
+			using (TripleDESCryptoServiceProvider tripleDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 }) {
+				string result = "";
+
+				byte[] byteDaCrypt = UTF8Encoding.UTF8.GetBytes(txt);
+
+				using (ICryptoTransform transform = tripleDes.CreateEncryptor()) {
+					byte[] results = transform.TransformFinalBlock(byteDaCrypt, 0, byteDaCrypt.Length);
+
+					result = Convert.ToBase64String(results, 0, results.Length);
+				}
+
+				return result;
+			}
+
+
+		}
+
+		/// <summary>
+		/// decripta il testo passatogli
+		/// </summary>
+		/// <param name="txt">da decriptare</param>
+		/// <returns>valore decriptato</returns>
+		public static string Decrypt_Text(string txt) {
+
+			if (string.IsNullOrEmpty(txt) || string.IsNullOrEmpty(PASSWORD))
+				return txt;
+			
+			byte[] keys;
+			using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider()) {
+				var tmp = UTF8Encoding.UTF8.GetBytes(PASSWORD);
+				keys = md5.ComputeHash(tmp);
+			}
+			
+			using (TripleDESCryptoServiceProvider tripleDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 }) {
+				string result = "";
+
+				////decripto
+
+				byte[] byteDaDeCrypt = Convert.FromBase64String(txt);
+
+				using (ICryptoTransform transform = tripleDes.CreateDecryptor()) {
+					byte[] results = transform.TransformFinalBlock(byteDaDeCrypt, 0, byteDaDeCrypt.Length);
+
+					result = UTF8Encoding.UTF8.GetString(results);
+				}
+
+				return result;
+
+			}
+		}
+
+	}
+}
